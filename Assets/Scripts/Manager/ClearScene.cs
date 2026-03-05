8c8965f3fd24e5229859060ec6994f982677a235
nakayama_yuto <udoyama5@gmail.com>
2026-03-05 19:00:15 +0900

    変更内容：クリアシーン内の花火パーティクルを実装した。

--- Assets/Scripts/Manager/ClearScene.cs
+++ Assets/Scripts/Manager/ClearScene.cs
@@ -8,6 +8,26 @@ namespace ForestDraw
     public class ClearScene : MonoBehaviour
     {
         /// <summary>
+        /// BGMオーディオ設定の変数
+        /// </summary>
+        [SerializeField]
+        private AudioSetting audioSettingBGM = null;
+        /// <summary>
+        /// SEオーディオ設定の変数
+        /// </summary>
+        [SerializeField]
+        private AudioSetting audioSettingSE = null;
+
+        /// <summary>
+        /// 初期設定の関数
+        /// </summary>
+        private void Start()
+        {
+            audioSettingSE.PlaySE(1);
+            audioSettingBGM.PlayBGM(4);
+        }
+
+        /// <summary>
         /// タイトルシーンへ遷移する関数
         /// </summary>
         public void InTitleScene()
--- Assets/Scripts/Manager/StageScene.cs
+++ Assets/Scripts/Manager/StageScene.cs
@@ -8,6 +8,26 @@ namespace ForestDraw
     public class StageScene : MonoBehaviour
     {
         /// <summary>
+        /// オーディオ設定の変数
+        /// </summary>
+        [SerializeField]
+        private AudioSetting audioSetting = null;
+
+        /// <summary>
+        /// BGMのインデックスの変数
+        /// </summary>
+        [SerializeField]
+        private int bgmIndex = 0;
+
+        /// <summary>
+        /// 初期設定の関数
+        /// </summary>
+        private void Start()
+        {
+            audioSetting.PlayBGM(bgmIndex);
+        }
+
+        /// <summary>
         /// クリアシーンへ遷移する関数
         /// </summary>
         public void InClearScene()
