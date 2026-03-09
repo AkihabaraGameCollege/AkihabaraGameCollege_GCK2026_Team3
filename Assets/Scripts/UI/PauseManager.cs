1c29a47f87e195d469780ba2e362cbda3d6b98ed
nakayama_yuto <udoyama5@gmail.com>
2026-03-09 15:02:14 +0900

    変更内容：ポーズ機能を実装した。

---
+++ Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace ForestDraw
+{ 
+    /// <summary>
+    /// ポーズUI
+    /// </summary>
+public class PauseManager : MonoBehaviour
+{
+        /// <summary>
+        /// 戻るボタンの変数
+        /// </summary>
+        [SerializeField]
+        private Button resumeButton = null;
+        /// <summary>
+        /// 設定ボタンの変数
+        /// </summary>
+        [SerializeField]
+        private Button settingButton = null;
+        /// <summary>
+        /// ステージセレクトボタンの変数
+        /// </summary>
+        [SerializeField]
+        private Button exitButton = null;
+
+        /// <summary>
+        /// ResumeButtonが押されたときに発生するUnityEventの変数
+        /// </summary>
+        public UnityEvent onResumeButtonClick = null;
+        /// <summary>
+        /// SettingButtonが押されたときに発生するUnityEventの変数
+        /// </summary>
+        public UnityEvent onSettingButtonClick = null;
+        /// <summary>
+        /// ExitButtonが押されたときに発生するUnityEventの変数
+        /// </summary>
+        public UnityEvent onExitButtonClick = null;
+
+        /// <summary>
+        /// 初期設定を行う関数
+        /// </summary>
+    void Awake()
+    {
+        // UnityEvent を追加
+        resumeButton.onClick.AddListener(() => { onResumeButtonClick.Invoke(); });// 戻るボタンのイベントを設定
+           settingButton.onClick.AddListener(() => { onSettingButtonClick.Invoke(); });// 設定ボタンのイベントを設定
+        exitButton.onClick.AddListener(() =>{ onExitButtonClick.Invoke(); });// ステージセレクトボタンのイベントを設定
+
+        Hide();// 起動時はUIを隠す
+    }
+
+    /// <summary>
+    /// UIを見せる関数
+    /// </summary>
+    public void Show()
+    {
+        // 子オブジェクトをすべてアクティブ化
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+
+    // このUIを非表示に設定します。
+    public void Hide()
+    {
+        // 子オブジェクトをすべて非アクティブ化
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+}
