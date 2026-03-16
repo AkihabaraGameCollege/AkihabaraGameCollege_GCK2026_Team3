fc0b947345f552be7dcf763370d26be329ab981c
nakayama_yuto <udoyama5@gmail.com>
2026-03-16 16:38:54 +0900

    変更内容：ゲームオーバーの機能を実装した。

--- Assets/Scripts/Manager/StageScene.cs
+++ Assets/Scripts/Manager/StageScene.cs
@@ -14,6 +14,17 @@ namespace ForestDraw
         private AudioSetting audioSetting = null;
 
         /// <summary>
+        /// ゲームオーバーUIの変数
+        /// </summary>
+        [SerializeField]
+        private GameObject gameOverUI = null;
+        /// <summary>
+        /// プレイヤー関係UIの変数配列
+        /// </summary>
+       [SerializeField]
+        private GameObject[] playerUI = null;
+
+        /// <summary>
         /// BGMのインデックスの変数
         /// </summary>
         [SerializeField]
@@ -24,6 +35,18 @@ namespace ForestDraw
         /// </summary>
         private void Start()
         {
+            // 配列内のゲームオブジェクトをすべて参照
+            foreach (GameObject obj in playerUI)
+            {
+                // オブジェクトがあった場合
+                if (obj != null) 
+                {
+                    obj.SetActive(true);
+                }
+            }
+
+            gameOverUI.SetActive(false);
+
             audioSetting.PlayBGM(bgmIndex);
         }
 
@@ -33,6 +56,24 @@ namespace ForestDraw
         public void InClearScene()
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("Clear");
+        }
+
+        /// <summary>
+        /// ゲームオーバーした際にゲームオーバー画面を表示する関数
+        /// </summary>
+        public void GameOver()
+        {
+            // 配列内のゲームオブジェクトをすべて参照
+            foreach (GameObject obj in playerUI)
+            {
+                // オブジェクトがあった場合
+                if (obj != null)
+                {
+                    obj.SetActive(false);
+                }
+            }
+
+            gameOverUI.SetActive(true);
         }
     }
 }
